d0e7daf12e6fe9e27da646d2ce7e8a960ce40df9
Galin0810 <galin08domain@gmail.com>
2018-11-12 22:25:03 +0200

    First commit

--- theaters/models.py
+++ theaters/models.py
@@ -11,12 +11,12 @@ class Theaters(db.Model):
     def __repr__(self):
         return '<Theaters {}>'.format(self.name)
 
-
 class Performance(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(140), index=True, unique=True)
-    description = db.Column(db.String(256), index=True, unique=True)
-    times = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    time = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    description = db.Column(db.String(256))
+    main_actors = db.Column(db.String(128))
     theaters_id = db.Column(db.Integer, db.ForeignKey('theaters.id'))
 
     def __repr__(self):
