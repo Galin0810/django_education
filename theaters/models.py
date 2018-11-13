c46516c4950afa315982c01084552fb022e0ab5f
Galin0810 <galin08domain@gmail.com>
2018-11-13 16:06:22 +0200

    first commit

--- theaters/models.py
+++ theaters/models.py
@@ -1,7 +1,7 @@
 from app import db
 from datetime import datetime
 
-class Theaters(db.Model):
+class Theater(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), index=True, unique=True)
     address = db.Column(db.String(120), index=True, unique=True)
@@ -9,15 +9,15 @@ class Theaters(db.Model):
     performance = db.relationship('Performance', backref='theater', lazy='dynamic')
 
     def __repr__(self):
-        return '<Theaters {}>'.format(self.name)
+        return '<Theater {}>'.format(self.name)
 
 class Performance(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(140), index=True, unique=True)
+    name = db.Column(db.String(140))
     time = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    description = db.Column(db.String(256))
-    main_actors = db.Column(db.String(128))
-    theaters_id = db.Column(db.Integer, db.ForeignKey('theaters.id'))
+    description = db.Column(db.String(200))
+    main_actors = db.Column(db.String(180))
+    theater_id = db.Column(db.Integer, db.ForeignKey('theater.id'))
 
     def __repr__(self):
         return '<Performance {}>'.format(self.name)
