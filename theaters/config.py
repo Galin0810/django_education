a011d4c74f076b045697b62e6e973bdd66b484c0
Galin0810 <galin08domain@gmail.com>
2018-11-28 10:40:53 +0200

    Final version

--- theaters/config.py
+++
@@ -1,7 +0,0 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-class Config(object):
-    # ...
-    SQLALCHEMY_DATABASE_URI =  'sqlite:///app.db?check_same_thread=False'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- theaters/venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/galina/PycharmProjects/theaters/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
