d80345e6e5822c34924ac3e7523e9dffc4a2ea12
Galin0810 <galin08domain@gmail.com>
2020-03-26 20:04:25 +0200

    Add chat with Chanel asyng

---
+++ chatDjango/chatDjango/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+
+from django.conf.urls import include
+from django.urls import path
+from django.contrib import admin
+
+urlpatterns = [
+    path('chat/', include('chat.urls')),
+    path('admin/', admin.site.urls),
+]
